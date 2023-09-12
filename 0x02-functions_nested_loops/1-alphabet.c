cb4d0d67b79546bf95989a50c86edb3b6bf4c805
iamtoxima <toximagithub@gmail.com>
2023-09-12 09:05:49 -0700

    alx

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,2 +1,16 @@
-#include "main.h"
-/*main - check the code 
+nclude "main.h"
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void)
+{
+char letter;
+
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+
+_putchar('\n');
+}
