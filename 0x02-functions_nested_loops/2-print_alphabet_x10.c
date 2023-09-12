ce8d55214da25f5043ada1b73524870b3d5475a9
iamtoxima <toximagithub@gmail.com>
2023-09-12 11:32:13 -0700

    ALX

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
-/**
- * print_alphabet_x10 - Prints the alphabet in lowercase ten times
- */
 void print_alphabet_x10(void)
 {
-    int i;
-    char letter;
+    int i, j;
 
     for (i = 0; i < 10; i++)
     {
-        for (letter = 'a'; letter <= 'z'; letter++)
+        for (j = 97; j < 123; j++)
         {
-            _putchar(letter);
+            _putchar(j);
         }
         _putchar('\n');
     }
