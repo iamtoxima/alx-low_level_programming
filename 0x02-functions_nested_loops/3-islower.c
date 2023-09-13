7a292d9bbfb54c7732f32a9770686da3169b0283
iamtoxima <toximagithub@gmail.com>
2023-09-13 14:56:53 -0700

    ALX

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
-* _islower - Checks if a character is lowercase
-* @c: The character to be checked
+*_islower - Checks if a character is lowercase
+*@c: The character to be checked
 *
-* Return: 1 if c is lowercase, 0 otherwise
- */
+*Return: 1 if c is lowercase, 0 otherwise
+*/
 int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
