4b7c636f566950f74a9411b24e6cd2a8aef263bb
iamtoxima <toximagithub@gmail.com>
2023-09-14 16:03:38 -0700

    ALX

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -8,7 +8,7 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-    /**
+/**
 *print_remaining_days - takes a date and prints how many days are
 *left in the year, taking leap years into account
 *@month: month in number format
