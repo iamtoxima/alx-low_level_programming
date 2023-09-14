df4aec4be862b065ca7772918bb4aa9b389b4e0a
iamtoxima <toximagithub@gmail.com>
2023-09-13 21:54:53 -0700

    alx

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,29 +1,29 @@
 #include <stdio.h>
 
 /**
-* main - Prints the first 98 Fibonacci numbers
-*
-* Return: Always 0
-*/
+ * main - Prints the first 98 Fibonacci numbers
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-unsigned long i, a = 1, b = 2, next;
+    unsigned long i, a = 1, b = 2, next;
 
-printf("%lu, %lu, ");
+    printf("1, 2, ");
 
-for (i = 3; i <= 98; ++i)
-{
-next = a + b;
-printf("%lu", next);
+    for (i = 3; i <= 98; ++i)
+    {
+        next = a + b;
+        printf("%lu", next);
 
-if (i != 98)
-printf(",");
+        if (i != 98)
+            printf(", ");
 
-a = b;
-b = next;
-}
+        a = b;
+        b = next;
+    }
 
-printf("\n");
+    printf("\n");
 
-return (0);
+    return 0;
 }
