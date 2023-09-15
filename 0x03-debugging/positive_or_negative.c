a2e3e1fdc9a72ce8fa2851dc997d31c69eb93a73
iamtoxima <toximagithub@gmail.com>
2023-09-15 14:17:25 -0700

    alx

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -2,11 +2,17 @@
 
 void postive_or_negative(int n)
 {
-  if (n < 0)
-    printf("%d is negative\n", n);
-  else if (n > 0)
-    printf("%d is positive\n", n);
-  else
-    printf("%d is zero\n", n);
-  return (0);
+if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+else if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else
+{
+printf("%d is zero\n", n);
+}
+return (0); /* Assuming you want to return 0 in all cases */
 }
