ddbb17ab23863ffc8fa2d6bdf7d48b58f4982c9b
iamtoxima <toximagithub@gmail.com>
2023-09-15 13:16:48 -0700

    ALX

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+void postive_or_negative(int n)
+{
+  if (n < 0)
+    printf("%d is negative\n", n);
+  else if (n > 0)
+    printf("%d is positive\n", n);
+  else
+    printf("%d is zero\n", n);
+  return (0);
+}
