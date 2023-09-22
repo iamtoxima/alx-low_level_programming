5af2ff0b6d998cc7e67b67e79e1e13120476e3b7
iamtoxima <toximagithub@gmail.com>
2023-09-22 15:05:47 -0700

    alx

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,23 +1,23 @@
 char *_strncat(char *dest, char *src, int n)
 {
-    int dest_len = 0;
-    int i = 0;
+int dest_len = 0;
+int i = 0;
 
-    /* Find the length of dest */
-    while (dest[dest_len] != '\0')
-    {
-        dest_len++;
-    }
+/* Find the length of dest */
+while (dest[dest_len] != '\0')
+{
+dest_len++;
+}
 
-    /* Append src to dest, up to n bytes or until null terminator is encountered in src */
-    while (src[i] != '\0' && i < n)
-    {
-        dest[dest_len + i] = src[i];
-        i++;
-    }
+/* Append src to dest, up to n bytes or until null terminator is encountered in src */
+while (src[i] != '\0' && i < n)
+{
+dest[dest_len + i] = src[i];
+i++;
+}
 
-    /* Add null terminator */
-    dest[dest_len + i] = '\0';
+/* Add null terminator */
+dest[dest_len + i] = '\0';
 
-    return dest;
+return dest;
 }
