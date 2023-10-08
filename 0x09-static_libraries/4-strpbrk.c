acf3da5c129154f32588e00eadeea637c5e8c8e0
iamtoxima <toximagithub@gmail.com>
2023-10-07 21:29:13 -0700

    alx task

--- 0x09-static_libraries/4-strpbrk.c
+++ 0x09-static_libraries/4-strpbrk.c
@@ -1,16 +1,18 @@
 #include "main.h"
+#include <stddef.h>
+
 char *_strpbrk(char *s, char *accept)
 {
 char *a = accept;
 
-while (*a != '\0') {
 while (*s != '\0') {
+while (*a != '\0') {
 if (*s == *a) {
 return s;
 }
-s++;
-}
 a++;
+}
+s++;
 }
 
 return NULL;
