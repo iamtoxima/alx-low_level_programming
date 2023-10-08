a09fbd7fd0a181b2defc51cb1e8a8fbe7e16d1ad
iamtoxima <toximagithub@gmail.com>
2023-10-08 15:03:24 -0700

    alx

---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+int _putchar(char);
+void *malloc_checked(unsigned int);
+char *string_nconcat(char *, char *, unsigned int);
+void *_calloc(unsigned int, unsigned int);
+int *array_range(int, int);
+void *_realloc(void *, unsigned int, unsigned int);
+
+#endif
