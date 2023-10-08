5d4b202f7460883faa1cffcb98819d971e983bf7
iamtoxima <toximagithub@gmail.com>
2023-10-07 21:01:58 -0700

    alx

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,12 +1,6 @@
-#include "main.h"
 #include <unistd.h>
-
-/**
-* _putchar - Writes a character to stdout
-* @c: The character to print
-* Return: 1 on success, -1 on error
-*/
 int _putchar(char c)
 {
-return (write(1, &c, 1) == 1 ? 1 : -1);
+/* Implementation of _putchar */
+return write(1, &c, 1);
 }
